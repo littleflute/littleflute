239bae3fa18b1352572a2ae7484577a87f0519ae
littleflute <littleflute@163.com>
2021-01-04 19:45:17 -0800

    1371

---
+++ i/1371/bldl.bat
@@ -0,0 +1,6 @@
+
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/bldl.bat	    		-o 		bldl.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/srt2v.bat	    	-o 		srt2v.bat
+curl https://raw.githubusercontent.com/littleflute/bat/master/bat/ffmpeg/gif2v.bat	    	-o 		gif2v.bat
+
+curl https://raw.githubusercontent.com/littleflute/blog/master/i/i1250/1138.gif		    	-o 		1138.gif
